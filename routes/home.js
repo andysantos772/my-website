e8298a0b47098d5edf389ff924c58c5ce1514ea3
Andy Santos <andysantos436@gmail.com>
2023-01-15 21:37:35 -0800

    Add '/' in home.js path

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '../pages/home');
+    res.sendFile(__dirname + '/../pages/home');
 });
 
 module.exports = router;
