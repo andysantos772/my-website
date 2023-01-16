10ce518258e73a10e1b5495006f4f07c2b7d6122
Andy Santos <andysantos436@gmail.com>
2023-01-15 21:30:35 -0800

    Revert routing to static html pages

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const home = require('./routes/home');
-const about = require('./routes/about');
 const express = require('express');
 const app = express();
 
@@ -12,7 +11,6 @@ mongoose.connect(db)
 
 app.use(express.json());
 app.use('/', home);
-app.use('about', about);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/about.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('../views/about');
-});
-
-module.exports = router;
