1bbfc83749d68ace3762514730ca1df5b75ed854
Andy Santos <andysantos436@gmail.com>
2023-01-15 23:04:58 -0800

    Try changing script path in html for MIME error

--- routes/scripts.js
+++ routes/scripts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/home', (req, res) => {
+router.get('/home.js', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'scripts', 'home.js'));
 });
 
