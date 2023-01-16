cef8b7fcbc790c29889cffcc1d74c71b7844f10c
Andy Santos <andysantos436@gmail.com>
2023-01-15 23:17:12 -0800

    Add public folder for static routing

--- routes/scripts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('/home.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'scripts', 'home.js'));
-});
-
-module.exports = router;
