84dfd20e8cc8d3a674f129dd20738367af456d9d
Andy Santos <andysantos436@gmail.com>
2023-01-15 23:36:24 -0800

    Revert "Serve all static pages with express"
    This reverts commit e12e8571f0fd0a99c66dfdf3e023d3b120514b89.

--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,7 @@ const container = document.querySelector('body');
 
 let a = document.createElement('a');
 a.textContent = 'About Link Created by JS';
-a.setAttribute('href', "./pages/about.html");
+a.setAttribute('href', "./about");
 
 container.appendChild(a);
 
