5d5fae27da525d634de8a15c697906f39f79239b
Andy Santos <andysantos436@gmail.com>
2023-01-15 17:52:35 -0800

    Add MongoDB Atlas database to environment variable

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const config = require('config');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/website')
+const db = config.get('db');
+mongoose.connect(db)
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
