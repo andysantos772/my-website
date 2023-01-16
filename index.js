1e989662a775338e1ca7ebbd58890fcf09e8dfc1
Andy Santos <andysantos436@gmail.com>
2023-01-15 21:04:10 -0800

    Add simple routes to test on Heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
+const home = require('./routes/home');
+const about = require('./routes/about');
 const express = require('express');
 const app = express();
 
@@ -9,6 +11,8 @@ mongoose.connect(db)
     .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
+app.use('/', home);
+app.use('about', about);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
