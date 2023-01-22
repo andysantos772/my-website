2d15531b32a702712e451d3f87c4fdf43f7772bf
Andy Santos <andysantos436@gmail.com>
2023-01-21 23:42:28 -0800

    Create the navigation bar
    The navbar is written to every page by loading the navbar.js
    file. The projects tab has a dropdown but every other link
    is direct. The already-written nav.js should take care of the routing.

--- scripts/home.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('body');
-
-let a = document.createElement('a');
-a.textContent = 'About Link Created by JS';
-a.setAttribute('href', "./about");
-
-container.appendChild(a);
-
-console.log('Success');
---
+++ scripts/navbar.js
@@ -0,0 +1,32 @@
+/* Write the navigation bar html, this script will be used in every page */
+document.write('\
+<nav> \
+  <a href="/home">HOME</a> \
+  <a href="/about">ABOUT</a> \
+  <div class="projects-block"> \
+    <a href="javascript:void(0)" id="projects">PROJECTS</a> \
+    <div class="project-dropdown"> \
+      <a href="/rpg">RPG CREATOR</a> \
+      <a href="">TEST</a> \
+    </div> \
+  </div> \
+  <a href="/contact">CONTACT</a> \
+</nav>');
+
+
+/* Code slightly modified from https://www.w3schools.com/howto/howto_js_dropdown.asp for projects dropdown menu*/
+function toggleShow() {
+    document.querySelector('.project-dropdown').classList.toggle('show');
+}
+
+document.getElementById('projects').addEventListener('click', toggleShow);
+
+document.addEventListener('click', function(event) {
+  if (!event.target.matches('#projects')) {
+    console.log('test');
+    const projectDropdown = document.querySelector('.project-dropdown');
+    if (projectDropdown.classList.contains('show')) {
+      projectDropdown.classList.remove('show');
+    }
+  }
+});
