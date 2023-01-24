e6abd31adeebdca5fd77ed291f6b5ed9dbcfe156
Andy Santos <andysantos436@gmail.com>
2023-01-23 16:51:43 -0800

    Add function to show image uploaded and some css

--- scripts/rpg.js
+++ scripts/rpg.js
@@ -10,3 +10,17 @@ function toggleForm() {
 document.querySelector('#stats').addEventListener('click', toggleForm);
 document.querySelector('#bio').addEventListener('click', toggleForm);
 document.querySelector('#spells').addEventListener('click', toggleForm);
+
+
+function showPortrait() {
+    const portrait = document.querySelector('#portrait');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+        portrait.src = reader.result;
+    };
+
+    if (file) reader.readAsDataURL(file);
+    else preview.src = "";
+}
