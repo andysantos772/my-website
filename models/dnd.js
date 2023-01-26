789a1ab4bae4545fdea7f44e014d26b95d08e8bd
Andy Santos <andysantos436@gmail.com>
2023-01-25 21:29:29 -0800

    Add current schema and validation code

--- models/dnd.js
+++ models/dnd.js
@@ -5,16 +5,47 @@ const mongoose = require('mongoose');
 const DndCharacter = mongoose.model('DndCharacter', new mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: true },
-  class: { type: String },
-  race: { type: String }
+  class: { type: String, trim:true },
+  race: { type: String, trim:true },
+  strength: { type: Number },
+  dexterity: { type: Number },
+  constitution: { type: Number },
+  intelligence: { type: Number },
+  wisdom: { type: Number },
+  charisma: { type: Number },
+  age: { type: String, trim:true },
+  height: { type: String, trim:true },
+  weight: { type: String, trim:true },
+  eyes: { type: String, trim:true },
+  skin: { type: String, trim:true },
+  hair: { type: String, trim:true },
+  allies: { type: String, trim:true },
+  backstory: { type: String, trim:true },
+  traits: { type: String, trim:true },
 }), 'dnd');
 
 // Experiment with what validation requirements work best
 const validateSchema = Joi.object({
-    name: Joi.string().min(1).max(100).required(),
-    class: Joi.string(),
-    race: Joi.string()
+    name: Joi.string().min(1).max(75).required(),
+    class: Joi.string().max(),
+    race: Joi.string(),
+    strength: Joi.number().integer().min(1).max(30),
+    dexterity: Joi.number().integer().min(1).max(30),
+    constitution: Joi.number().integer().min(1).max(30),
+    intelligence: Joi.number().integer().min(1).max(30),
+    wisdom: Joi.number().integer().min(1).max(30),
+    charisma: Joi.number().integer().min(1).max(30),
+    age: Joi.string().max(15),
+    height: Joi.string().max(10),
+    weight: Joi.string().max(10),
+    eyes: Joi.string(),
+    skin: Joi.string(),
+    hair: Joi.string(),
+    allies: Joi.string(),
+    backstory: Joi.string(),
+    traits: Joi.string(),
 });
 
 exports.DndCharacter = DndCharacter;
