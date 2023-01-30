dab5e18b972370366f3bbe2f142f59dd76e0e056
Andy Santos <andysantos436@gmail.com>
2023-01-29 18:36:08 -0800

    Add more stat blocks

--- scripts/rpg.js
+++ scripts/rpg.js
@@ -11,7 +11,7 @@ document.querySelector('#stats').addEventListener('click', toggleForm);
 document.querySelector('#bio').addEventListener('click', toggleForm);
 document.querySelector('#spells').addEventListener('click', toggleForm);
 
-
+// Preview the image the user uploaded
 function showPortrait() {
     const portrait = document.querySelector('#portrait');
     const file = document.querySelector('input[type=file]').files[0];
@@ -24,3 +24,27 @@ function showPortrait() {
     if (file) reader.readAsDataURL(file);
     else preview.src = "";
 }
+
+// Submit the forms to the database
+function submitForms() {
+    //
+}
+
+function submitInfo() {
+    //
+}
+
+function submitStats() {
+    //
+}
+
+function submitBio() {
+    //
+}
+
+function submitSpells() {
+    //
+}
+
+
+// Add spells to the spell list
